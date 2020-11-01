e29ba20a9dc13100077551c6f1cfa7903f904195
adanxx <dan_47i@hotmail.com>
2020-11-01 14:32:01 +0100

    Completed Animation Section

--- shared.js
+++ shared.js
@@ -4,7 +4,8 @@
  var modal = document.querySelector('.modal');
  var modalButton = document.querySelector('.modal__action--negative');
  var toggleButton =document.querySelector('.toggle-button');
- var mobileNav =document.querySelector('.mobile-nav')
+ var mobileNav =document.querySelector('.mobile-nav');
+ var ctaButton = document.querySelector('.main__nav--cta');
 
   for (var i = 0 ; i < selectPlanButton.length ; i++){
     selectPlanButton[i].addEventListener('click', function(){
@@ -38,3 +39,11 @@
     mobileNav.classList.add('open_mobile')
     backdrop.classList.add('open');
   })
+
+  ctaButton.addEventListener('animationstart', function(){
+    console.log('Animation Start', Event);
+  })
+
+  ctaButton.addEventListener('animationend', function(){
+    console.log('Animation End', Event);
+ })
